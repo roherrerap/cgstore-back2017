611dd830b0ccc7fd9bd2f150c9e3a0a8bcb3b8d3
AceMaster <ace7ace@hotmail.com>
2017-11-02 20:05:54 -0500

    new_quote controller and methods added

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :attributeproducts
   resources :features
   resources :products
+  resources :new_quote
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
