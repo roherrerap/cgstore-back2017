6cdc06561a5da6f6b2d049abdb84dffbb915782c
LAPTOP-B91FCI08\Noctis <roherrerap@unal.edu.co>
2017-11-02 20:15:15 -0500

    Update - Consu

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :features
   resources :products
   resources :new_quote
+  resources :consultproducts
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
