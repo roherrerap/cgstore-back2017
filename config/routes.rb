0d97c91f2b170fb9a9964bb192e795309d48cbd9
LAPTOP-B91FCI08\Noctis <roherrerap@unal.edu.co>
2017-11-13 10:01:07 -0500

    Update - controller to check consult

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :consultproducts
   resources :automatizations
   resources :user_quote
+  resources :heavyequipments
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
